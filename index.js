322d2a4087e2818865a1437cfa6487357c2c8a27
AHADKHAN2202 <101731915+AHADKHAN2202@users.noreply.github.com>
2022-11-30 01:40:24 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 
+//  dom practice what is taught by masoom sir
+
 // let obj = document.getElementsByClassName('heading');
 // let obj1 = document.getElementById('unorder-list');
    
@@ -17,6 +19,7 @@
 // console.log(document);
 
 
+// dom lecture 1 practice by prakash sir date:28-11-2022
 
 // console.log("Hello");
 
@@ -35,7 +38,7 @@
 // console.log("Hello");
 
 
-// console.log(document); // document is a html file inside document we have all html code.
+// console.log(document); // document is a html file, inside document we have all html code.
 
 // const selector1 = document.getElementById('para2'); // it will select the element in html which has a id of para2
 // console.log(selector1,"it is selected");
@@ -46,9 +49,45 @@
 // const selector3 = document.getElementsByTagName('div'); // it will select all the tagname provided in parameters.
 // console.log(selector3,'selected'); // it will give html collection like an array. but why when give id to tag it is showing a list?
 
-// const selector4 = document.querySelector('.para1','#para2');
-// console.log(selector4,"selected"); doubts1: when we select it is giving all element inside div but not in using class name.
-//doubt2: when we are providing in selector id it showing in list why?
+// const selector4 = document.querySelector('#para2');
+// console.log(selector4,"selected"); //doubts1: when we select it is giving all element inside div but not in using class name.
+ //                                  doubt2: when we are providing in selector id it showing in list why?
 
 // const selector5 = document.querySelectorAll('.para1,#para2,div'); // it will select the multiple element inside it either class name,id name and tag name.return node list like an array.
 // console.log(selector5,'selected');
+
+// console.log(document.body);
+
+
+
+// lecture 2 practice by kamal sir date:29-11-2022
+
+// console.log(document); // why it is showing paragraph tag? when i use script tag in head.
+// const selector1 = document.querySelector("#id1,#id2,#id3");
+// console.log(selector1);
+
+// console.log(document.getElementById('id1'));
+//  const ele = document.getElementById('id1');
+// //   console.log(ele.style); // it will give the all the css property for the element.
+// console.log( ele.style.fontSize = '50px') // it will change the css of element using js 
+// ele.style.color = 'red';
+// ele.style.backgroundColor = 'yellow'
+
+// if we want to give multiple style--
+
+// const mulStyle = {
+//     fontSize:'60px',
+//     backgroundColor:"red",
+//     color:"white"
+// }
+
+// Object.assign(ele.style,mulStyle);
+const ele = document.getElementById('some-para');
+const ul = document.getElementById('uno');
+// const body = document.getElementById('body');
+// console.log(body.innerText);
+// console.log(body.innerHTML);
+// console.log(body.outerHTML);
+
+// ele.innerHTML = ul.innerHTML;
+ele.innerHTML = ul.outerHTML
